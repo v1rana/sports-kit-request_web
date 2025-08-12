4acbd7561135d380b23c175426f58034cff4646d
vijay <vijaysainisemt@gmail.com>
2025-08-12 13:55:52 +0530

    build

--- resources/js/services/api.ts
+++ resources/js/services/api.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL+'/api';
 
+console.log('API_BASE_URL',API_BASE_URL);
+
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
